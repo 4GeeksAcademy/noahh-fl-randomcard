3ebc68bceef816840866dd1a57e3dcf128554ca9
noahh-fl <18nflewelling@gmail.com>
2025-01-30 01:33:07 +0000

    final random card genorator

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+};
+
+window.onload = function() {
+  const suits = ["♥", "♠", "♣", "♦"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.querySelector(".top-left").innerHTML = randomSuit;
+  document.querySelector(".middle").innerHTML = randomNumber;
+  document.querySelector(".bottom-right").innerHTML = randomSuit;
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    document
+      .querySelectorAll(".suit")
+      .forEach(suit => (suit.style.color = "red"));
+  } else {
+    document
+      .querySelectorAll(".suit")
+      .forEach(suit => (suit.style.color = "black"));
+  }
 };
